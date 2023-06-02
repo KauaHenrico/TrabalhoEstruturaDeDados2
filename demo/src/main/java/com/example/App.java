d6a0f0c2ac99847a76adc303c0931165f3a40774
KauaHenrico <108253678+KauaHenrico@users.noreply.github.com>
2023-06-02 00:00:28 -0300

    T2 feito (falta algumas correções.)

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,34 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
+         // Testando VerificadorPalindromo
+         String palavra = "arara";
+         boolean resultado = VerificadorPalindromo.isPalindromo(palavra);
+         System.out.println("A palavra \"" + palavra + "\" é um palíndromo? " + resultado);
+ 
+         // Testando PilhaAlunos e FilaAlunos
+         PilhaAlunos pilha = new PilhaAlunos();
+         FilaAlunos fila = new FilaAlunos();
+ 
+         // Inserindo alunos na pilha
+         pilha.empilhar(new Aluno("João", 20));
+         pilha.empilhar(new Aluno("Maria", 22));
+         pilha.empilhar(new Aluno("Pedro", 19));
+ 
+         // Imprimindo o estado da pilha
+         pilha.imprimirEstado();
+ 
+         // Invertendo a ordem dos alunos da fila usando a pilha
+         while (!pilha.estaVazia()) {
+             Aluno aluno = pilha.desempilhar();
+             fila.enfileirar(aluno);
+         }
+ 
+         // Imprimindo o estado da fila
+         fila.imprimirEstado();
+     }
 }
+
---
+++ demo/src/main/java/com/example/FilaAlunos.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FilaAlunos {
+    private Queue<Aluno> alunos;
+
+    public FilaAlunos() {
+        alunos = new LinkedList<>();
+    }
+
+    public void enfileirar(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+    public Aluno desenfileirar() {
+        if (estaVazia()) {
+            throw new IllegalStateException("A fila está vazia.");
+        }
+        return alunos.poll();
+    }
+
+    public boolean estaVazia() {
+        return alunos.isEmpty();
+    }
+
+    public void imprimirEstado() {
+        System.out.println("Estado da fila de alunos:");
+        for (Aluno aluno : alunos) {
+            System.out.println("Nome: " + aluno.getNome() + ", Idade: " + aluno.getIdade());
+        }
+    }
+}
